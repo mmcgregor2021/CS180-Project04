93ebce482e1c27814e96bddb194b0487a1d522bf
Kris Leungwattanakij <62641870+krislwk@users.noreply.github.com>
2021-11-08 21:11:52 -0500

    Added methods for parsing txt's

--- dataPersistenceMethods.java
+++ dataPersistenceMethods.java
@@ -38,9 +38,49 @@ public class dataPersistenceMethods {
         }
     }
 
+    //reads student objects from the txt file created by saveStudents() and returns and ArrayList of students
+    public static ArrayList<Student> readStudents(String fileName) {
+        ArrayList<Student> arr = new ArrayList<>();
+        try (BufferedReader bfr = new BufferedReader(new FileReader(fileName))) {
+            String line = bfr.readLine();
+            while (line != null) {
+                String[] lineArr = line.split(";");
+                String firstName = lineArr[0];
+                String lastName = lineArr[1];
+                String password = lineArr[2];
+                int id = Integer.parseInt(lineArr[3]);
+                arr.add(new Student(firstName, lastName, password, id));
+                line = bfr.readLine();
+            }
+        } catch (Exception e) {
+            System.out.println("Failed to parse text file!");
+        }
+        return arr;
+    }
+
+    //reads teacher objects from the txt file created by saveTeachers() and returns and ArrayList of teachers
+    public static ArrayList<Teacher> readTeachers(String fileName) {
+        ArrayList<Teacher> arr = new ArrayList<>();
+        try (BufferedReader bfr = new BufferedReader(new FileReader(fileName))) {
+            String line = bfr.readLine();
+            while (line != null) {
+                String[] lineArr = line.split(";");
+                String firstName = lineArr[0];
+                String lastName = lineArr[1];
+                String password = lineArr[2];
+                int id = Integer.parseInt(lineArr[3]);
+                arr.add(new Teacher(firstName, lastName, password, id));
+                line = bfr.readLine();
+            }
+        } catch (Exception e) {
+            System.out.println("Failed to parse text file!");
+        }
+        return arr;
+    }
+
+
     //Main method for testing purposes
     public static void main(String[] args) {
         
     }
-
 }
