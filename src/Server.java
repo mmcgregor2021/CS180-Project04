10b2a714afabbb35dd79cde537f1c6ab3c70f0bf
Kris Leungwattanakij <62641870+krislwk@users.noreply.github.com>
2021-12-02 20:35:43 -0500

    imported methods from TestClient into GUI.java

--- src/Server.java
+++ src/Server.java
@@ -294,5 +294,65 @@ public class Server {
         return arr;
     }
 
+    public static void signUp(Integer userID, String firstName, String lastName, String password, String role, Socket socket) {
+        try {
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String signUpPayload = "signup;" + userID + ";" + password + ";" + role + ";" + firstName + ";" + lastName;
+            out.println(signUpPayload);
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Integer requestNewID(Socket socket) {
+        try {
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            //sending a request to the server for a new ID to be assigned
+            out.println("newID");
+            out.flush();
+            return Integer.parseInt(in.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    //TODO replace print statements with JOptionPane windows
+    public static void logIn(Integer userID, String password, Socket socket) {
+        try {
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String verificationPayload = "login;" + userID + ";" + password;
+            out.println(verificationPayload);
+            out.flush();
+            switch(Integer.parseInt(in.readLine())) {
+                //TO DO: replace print statements below with JOptionPane messages
+                case 1:
+                    System.out.println("ID DOESN'T EXIST");
+                    break;
+                case 2:
+                    System.out.println("WRONG PASSWORD");
+                    break;
+                case 3:
+                    System.out.println("SUCCESFUL LOGIN");
+                    break;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void sendRequest(String requestPayload, Socket socket) {
+        try {
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            out.println(requestPayload);
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
